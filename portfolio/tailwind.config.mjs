e683523f9a39c20b62ad068cca1e29d63226a388
asjadahamed77 <asjadahamed77@gmail.com>
2025-02-05 18:07:29 +0530

    Created Navbar

--- portfolio/tailwind.config.mjs
+++ portfolio/tailwind.config.mjs
@@ -8,9 +8,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f',
       },
+      fontFamily: {
+        ovo: ['Ovo', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      }
     },
   },
   plugins: [],
