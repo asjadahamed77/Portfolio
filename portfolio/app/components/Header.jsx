b9f4c661742e10b5ad480022b8c0e2d9e04192dd
asjadahamed77 <asjadahamed77@gmail.com>
2025-02-16 16:52:26 +0530

    updated resume

--- portfolio/app/components/Header.jsx
+++ portfolio/app/components/Header.jsx
@@ -62,7 +62,7 @@ const Header = ({ isDarkMode, setIsDarkMode }) => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.8, delay: 1.2 }}
-          href="/sample-resume.pdf"
+          href="/Asjad-CV.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 cursor-pointer dark:bg-white  dark:text-black"
         >
